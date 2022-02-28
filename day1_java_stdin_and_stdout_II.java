8344df92ee20061939dcc40f1cf220f05bbb4c9b
INSAN BATRIA_D0221384_A <100111169+batriainsan@users.noreply.github.com>
2022-02-28 12:19:46 +0800

    Add files via upload

---
+++ day1_java_stdin_and_stdout_II.java
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+
+
+
+
+
+
+
+
+
+    // Declare second integer, double, and String variables.
+
+    int a;
+    double b;
+    string c;
+    // Read and save an integer, double, and String to your variables.
+    cin>>a>>b;
+    getline(cin>>ws,c);
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    cout<<(i+a)<<endl;
+    // Print the sum of the double variables on a new line.
+    cout<<fixed<<setprecision(1)<<(d+b)<<endl;
+    // Concatenate and print the String variables on a new line
+    cout<<fixed<<setprecis    // Concatenate and print the String variables on a new line
+ion(1)<<(d+b)<<endl;
+    // The 's' variable above should be printed first.
+    cout<<s+c;
+    return 0;
+}
